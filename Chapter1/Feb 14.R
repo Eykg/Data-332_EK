3f5b9a700eaae70f6d20e2cc754ad7e5696b8afe
Emma K <159856425+Eykg@users.noreply.github.com>
2024-02-14 20:36:29 -0600

    Chapter1

---
+++ Chapter1/Feb 14.R
@@ -0,0 +1,22 @@
+# Exercise 1
+chosen_number <- 5
+result_1 <- chosen_number + 2
+
+result_1
+
+
+# Exercise 2
+result_2 <- result_1 * 3
+
+result_2
+
+# Exercise 3
+result_3 <- result_2 - 6
+
+
+# Exercise 4
+result_4 <- result_3 / 3
+
+result_4
+
+
